65fbeac0aa58c9bfdd1516bca3c71fa3976242d6
Mohamed EL BERMIL <89483382+MohamedELBERMIL69780@users.noreply.github.com>
2023-04-25 15:41:36 +0200

    Add files via upload

---
+++ commun/header.php
@@ -0,0 +1,17 @@
+<header>
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+    
+            <a href="index.php" class="navbar-brand text-danger">Drive-X</a>
+        
+            <ul class="navbar-nav">
+                <li class="nav-item" >
+                    <a href="cars.php" class="nav-link">Voitures</a>
+                </li>
+                <li class="nav-item" >
+                    <a href="drivers.php" class="nav-link">Pilotes</a>
+                </li>
+            </ul>
+        
+        </nav>
+
+    </header>
